e6ad0ba087994db672ccc2017358e44caef2c8b3
willr520 <willr520@newschool.edu>
2023-11-08 16:11:20 -0500

    node practice

--- test1.js
+++ test1.js
@@ -1,1 +1,18 @@
-console.log("HELLO")
+// Import the express library and assign it to a variable
+import express from 'express'
+
+// Create an instance of an express application 
+const app = express()
+
+// Set the port the application will be running on
+const port = process.env.PORT || 3001
+
+// Set up a response for the root path of the application
+app.get('/', (req, res) => {
+  res.send("draw your favorite animal")
+})
+
+// Set the application to listen a port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}` )
+})
